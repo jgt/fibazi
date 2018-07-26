b3f1369bde220109f6f2cdc41e6011f6c864a029
jgt <jgt08@hotmail.com>
2018-07-26 09:32:13 -0500

    nuevas vistas

--- routes/route_app.js
+++ routes/route_app.js
@@ -2,19 +2,32 @@ const express = require("express");
 const email = require("../controllers/sendEmail");
 const route = express.Router();
 
+//Index
 route.get("/", function(req, res){
 	res.render("pug/pages/index");
 	//res.render("pug/pages/503-page");
 });
-
+//Blog
 route.get("/blog", function(req, res){
 	res.render("pug/pages/classic-blog");
 });
-
+//Contact
 route.get("/contact", function(req, res){
 	res.render("pug/pages/contacts");
 });
-
+//POST Contact
 route.post("/contact", email.sendEmail);
+//Login
+route.get("/login", function(req, res){
+	res.render("login/index");
+});
+
+//Sobre Fibazi
+route.get("/antecedentes", function(req, res){
+	res.render("pug/pages/antecedentes")
+});
+route.get("/sobreFibazi", function(req, res){
+	res.render("pug/pages/aboutFibazi");
+});
 
 module.exports = route;
