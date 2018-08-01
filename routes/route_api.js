68ad889af59d4372116724b81642d833de1787b8
jgt <jgt08@hotmail.com>
2018-08-01 13:49:43 -0500

    modificacion propiedades fibazi

--- routes/route_api.js
+++ routes/route_api.js
@@ -3,10 +3,6 @@ const route_api = express.Router();
 const logged = require("../middleware/logged");
 const api = express.Router();
 
-//Constrollers
-const user = require("../controllers/createUser");
-
-
 //Authenticate
 api.get('/logged', logged, function(req, res){
 	res.send('usuario logeado');
@@ -15,11 +11,6 @@ api.get('/logged', logged, function(req, res){
 //Error vista
 api.get('/error', logged, function(req, res){
 	res.render('pug/pages/503-page');
-});
-
-//Create User
-api.get("/user", logged, user.createUser, function(req, res){
-	res.render("pug/pages/index");
 });
 
 module.exports = api;
---
+++ seeder/users-seeder.js
@@ -0,0 +1,31 @@
+const User = require('../models/user');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://fibazi:jgt3556792@ds141641.mlab.com:41641/fibazi');
+
+const users = [
+	new User({
+		nombre: "jair",
+		email: "jgt08@hotmail.com",
+		password: "3556792"
+	}),
+	new User({
+		nombre: "angela",
+		email: "angela@hotmail.com",
+		password: "3556792"
+	})
+];
+
+var done = 0;
+for (var i = 0;	i < users.length; i++) {
+	users[i].save(function(err, result){
+		done++;
+		if(done === users.length){
+			exit();
+		}
+	})
+}
+
+function exit(){
+	mongoose.disconnect();
+}
